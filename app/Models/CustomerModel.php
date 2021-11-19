4590f8f0b40e875bf47775f45215236083eab8f0
Jeremy <adgrygry12348@gmail.com>
2021-11-19 15:01:07 +0700

    Rename UserController to CustomerController

--- app/Models/CustomerModel.php
+++ app/Models/CustomerModel.php
@@ -5,8 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Auth\User as Authenticates;
 
-class CustomerModel extends Model
+class CustomerModel extends Authenticates
 {
     use HasFactory;
     use SoftDeletes;
@@ -15,12 +16,13 @@ class CustomerModel extends Model
     public $primaryKey  = "id_customer";
     public $incrementing= true;
     public $timestamps  = true;
-    protected $fillable = ['firstName','lastName','username','email','address','kota','kodeZip','created_at','updated_at','deleted_at'];
+    protected $fillable = ['username','firstName','lastName','email','address','kota','kodeZip'];
+    protected $hidden = ['password'];
 
     public function saveData($username,$firstname,$lastname,$email,$address,$kota,$kodezip,$password){
         $customer                 = new CustomerModel();
-        $customer->id_customer    = null;
-        $customer->firstName      = $firstname; 
+        //$customer->id_customer    = null;
+        $customer->firstName      = $firstname;
         $customer->lastName       = $lastname;
         $customer->username       = $username;
         $customer->email          = $email;
@@ -28,9 +30,9 @@ class CustomerModel extends Model
         $customer->kota           = $kota;
         $customer->kodeZip        = $kodezip;
         $customer->password       = $password;
-        $customer->created_at     = null;
-        $customer->updated_at     = null;
-        $customer->deleted_at     = null;
+        //$customer->created_at     = null;
+        //$customer->updated_at     = null;
+        //$customer->deleted_at     = null;
         $customer->save();
     }
 }
