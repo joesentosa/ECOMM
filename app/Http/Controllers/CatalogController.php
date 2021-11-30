3129108a7202529b79829093925dfce7d78a1fc4
Dtake88 <56423564+Dtake88@users.noreply.github.com>
2021-12-01 00:27:32 +0700

    membenarkan

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -17,11 +17,17 @@ class CatalogController extends Controller
         $dtbrand    = BrandModel::limit(5)->get();
         $dtbarang   = BarangModel::get();
         $customer   = Auth::user();
+        $WLCek      = null;
+        $WLCust      = null;
         // dd($customer);
-        $WLCek = WishlistModel::where('fk_id_customer',$customer->id_customer)->first();
+        if ($customer == null) {
+            $customer = null;
+        }else{
+            $WLCek = WishlistModel::where('fk_id_customer',$customer->id_customer)->first();
+        }
         $WishlistCust = null;
         $wishlistCount = 0;
-        if ($WLCek) {
+        if ($WLCek != null) {
             $WishlistCust = WishlistModel::where('fk_id_customer',$customer->id_customer)->get();
             $wishlistCount = count($WishlistCust);
             $WLCust = BarangModel::whereIn('id_barang',$WishlistCust)->get();
